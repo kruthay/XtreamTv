8fedd7df016c4d45ba4710a83ef94272d34ef6c1
kruthay <kruthay.donapati@revomo.com>
2025-03-04 19:33:43 -0500

    Initial Commit

---
+++ XtreamTv/XtreamTvApp.swift
@@ -0,0 +1,17 @@
+//
+//  XtreamTvApp.swift
+//  XtreamTv
+//
+//  Created by Kruthay Kumar Reddy Donapati on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct XtreamTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
